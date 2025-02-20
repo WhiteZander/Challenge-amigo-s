730e7ab42366da000a62f24770df23b72ff2f70e
Alberto Rubio <jarceballos@gmail.com>
2025-02-20 16:15:08 -0600

    Añade función agregarAmigo

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 //Inicializar las variables
 
 let nombreAmigo;
-let listaNombres=[];
+let listaNombres= [];
+
+//Funciones
+
+function agregarAmigo (){
+
+    nombreAmigo=document.getElementById("amigo").value;
+    listaNombres.push(nombreAmigo);
+    console.log(nombreAmigo)
+    console.log(listaNombres)
+
+
+}
+
+
 
