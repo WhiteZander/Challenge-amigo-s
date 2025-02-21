1d7442295458696f8283a0dd40ce1ee8aef3588e
Alberto Rubio <jarceballos@gmail.com>
2025-02-20 21:28:13 -0600

    Termina lista de nombres y empieza funcion sortear

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 //Inicializar las variables
 
-let nombreAmigo;
+let nombreAmigo="";
 let listaNombres= [];
+let listaHTML="";
 
-//Funcion agregar, verifica campo vacio y limpia nombre
+//Funcion agregar, verifica campo vacio, limpia nombre y agrega a HTML 
 
-function limpiarNombre(){
-    document.getElementById("amigo").value = '';
- 
- }
+
+
+
 
 function agregarAmigo (){
+
 
     nombreAmigo=document.getElementById("amigo").value.trim();
 
@@ -19,17 +20,54 @@ function agregarAmigo (){
         } else {
             asignarTextoElemento('listaAmigos','');
             listaNombres.push(nombreAmigo);
-            console.log(nombreAmigo)
-            console.log(listaNombres)
-            asignarTextoElemento('listaAmigos', listaNombres);
+            console.log(nombreAmigo);
+            console.log(listaNombres);
             limpiarNombre();
-
+            agregarListaHtml();
+            console.log(listaHTML);     
         }
+
 }
 
+//Funciones complementarias
 
 function asignarTextoElemento (elemento, texto) {
     let elementoHTML = document.getElementById(elemento);
     elementoHTML.innerHTML = texto; 
 }
 
+function removerLista(){
+    asignarTextoElemento('listaAmigos','');
+}
+
+function agregarListaHtml (){
+
+    for ( let i=0; i< listaNombres.length; i++) {
+       listaHTML+= `<li> ${listaNombres[i]}</li>`
+       console.log(listaNombres[i])
+    }
+
+    asignarTextoElemento('listaAmigos', listaHTML);  
+
+    return listaHTML="";
+}
+
+function limpiarNombre(){
+    document.getElementById("amigo").value = '';
+ 
+ }
+
+
+
+//Sortea nombre y revisa que array no esté vacío
+
+
+function sortearAmigo(){
+    if(listaNombres.length<=0){
+
+        alert("Ingresa al menos un nombre para sortear");
+    } else {
+
+
+    }
+}
